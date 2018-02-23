9253f333736140cbf69c1d6519730430df8963f8
dhk <dahbka96@gmail.com>
2018-02-23 19:06:50 +0500

    default?

---
+++ Software Engineering/Assets/Scripts/dahbka/ObjectPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objpull : MonoBehaviour
+{
+
+    private GameObject[] objprefabs;
+    public GameObject Getobj(string name)
+    {
+        for (int i = 0; i < objprefabs.Length; i++)
+        {
+            if (objprefabs[i].name == name)
+            {
+                GameObject newObject = Instantiate(objprefabs[i]);
+                newObject.name = name;
+                return newObject;
+            }
+        }
+        return null;
+    }
+}
