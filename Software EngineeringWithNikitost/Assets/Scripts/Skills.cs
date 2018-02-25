b07e0631c18b075430e9bb241be26e53280f36fd
nikitost <nikitost@users.noreply.github.com>
2018-02-25 21:16:53 +0500

    Levet Plus Character

---
+++ Software EngineeringWithNikitost/Assets/Scripts/Skills.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Skills : MonoBehaviour {
+
+	public string NickName;
+	public int Max_HP;
+	public float Jump_Force;
+	public float Speed;
+	public float Weight;
+
+    /*public void firstSKill()
+    {
+        curHP = 100;
+        speed = 40;
+    }
+    public void thirdSKill()
+    {
+        curHP = 100;
+    }*/
+    
+    
+    
+}
